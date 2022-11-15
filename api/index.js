5d91e84b9edfb4346c3e35b8188eb1150bbf53fa
vitsmart <vitsmart2@gmail.com>
2022-11-15 21:20:47 +0100

    commit

---
+++ api/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
+
+
+dotenv.config();
+app.use(express.json());
+app.use(cors());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
+
+
+mongoose.connect(
+    process.env.MONGO_URL)
+    .then(() => console.log("DB connection succesful!"))
+    .catch((err) => console.log(err));
+const Port = 5000;
+app.listen(Port, () => {
+    console.log(`Backend server is running on ${Port}`)
+})
